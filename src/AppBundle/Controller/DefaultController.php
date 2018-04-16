c572068e049b99f619ca193552a93a3cdf363959
GuillaumeD84 <guillaume.durand@outlook.fr>
2018-04-16 18:12:53 +0200

    add pages in the controller + generic templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,44 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/skills", name="skills")
+     */
+    public function indexSkills()
+    {
+        return $this->render('default/skills.html.twig');
+    }
+
+    /**
+     * @Route("/formation", name="formation")
+     */
+    public function indexFormation()
+    {
+        return $this->render('default/formation.html.twig');
+    }
+
+    /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function indexPortfolio()
+    {
+        return $this->render('default/portfolio.html.twig');
+    }
+
+    /**
+     * @Route("/aboutme", name="aboutme")
+     */
+    public function indexAboutme()
+    {
+        return $this->render('default/aboutme.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function indexContact()
+    {
+        return $this->render('default/contact.html.twig');
+    }
 }
